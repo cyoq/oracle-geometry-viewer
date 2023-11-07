cec055d72918edfb21958f8b839495e5b837ccd8
cyoq <di9akoz@gmail.com>
2023-11-07 15:50:13 +0200

    Add BadRequest when Oracle gives error

--- ora_geom_api/main.py
+++ ora_geom_api/main.py
@@ -105,7 +105,13 @@ async def geometry(request: GeometryRequest) -> list[SdoGeometry]:
             detail="Only SELECT statements can be used in a query",
         )
 
-    data: list = execute_sql(request.sql)
+    try:
+        data: list = execute_sql(request.sql)
+    except oracledb.DatabaseError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Database error: {exc}",
+        )
 
     match validate_data(data):
         case Validation.EmptySet:
